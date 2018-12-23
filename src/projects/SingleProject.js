2dc7a975e85aa5b1e79fe4c3af68ca3d64b49418
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-12-23 11:14:40 -0500

    images not disappearing on hover

--- src/projects/SingleProject.js
+++ src/projects/SingleProject.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import './SingleProject.css';
 
+
+
 class SingleProject extends Component {
 
   render() {
+
     return (
       <div className="project-display">
+        <div className="project-image">
+          <img src={this.props.project.image} alt={this.props.project.alt}></img>
+        </div>
+        <div>
           <h2 className="emphasize project-title">{this.props.project.name}</h2>
           <p className="project-description">{this.props.project.description}</p>
           <div className="project-links">
-            <a className="links" target="_blank" rel="noopener noreferrer" href={this.props.project.deployedUrl}>[ Deployed ]</a>
-            <a className="links" target="_blank" rel="noopener noreferrer" href={this.props.project.githubUrl}>[ GitHub ]</a>
+              <a className="links-light-gray" target="_blank" rel="noopener noreferrer" href={this.props.project.deployedUrl}>[ Deployed ]</a>
+              <a className="links-light-gray" target="_blank" rel="noopener noreferrer" href={this.props.project.githubUrl}>[ GitHub ]</a>
           </div>
+        </div>
       </div>
     );
   }
