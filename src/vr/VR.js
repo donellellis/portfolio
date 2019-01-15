ab030fa2ab892734d419488a7b0ab2d49cfd9061
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2019-01-15 14:01:54 -0500

    vf page responsive

--- src/vr/VR.js
+++ src/vr/VR.js
@@ -9,12 +9,12 @@ class VR extends Component {
       <div className="vr-container">
           <div className="vr-container-column">
             <h1 className="vr-title">VR.</h1>
-            <div className="textBox">
+            <div className="vr-textBox">
               <h2 className="emphasize">Moonshot</h2>
               <h3>Personal Design Project</h3>
               <p>Moonshot is an unbuilt, personal interior design project for a tech start-up. Developers can have heads-down work time in the closed benching area, step out for a private call, or host meet-up events and client presentations in the residential-inspired lounge and collaboration room. Click on the VR icon on desktop to open fullscreen or on mobile with viewer for an immersive experience.
               </p>
-              <p>
+              <p className="vr-tech">
               Tech: Revit | Enscape3D | A-Frame
               </p>
             </div>
