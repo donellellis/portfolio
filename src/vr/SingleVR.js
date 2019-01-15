4ed6af529b9c38af7322aa65790f48539122f10f
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2019-01-15 11:30:46 -0500

    a-frame added to vr page

--- src/vr/SingleVR.js
+++ src/vr/SingleVR.js
@@ -5,7 +5,7 @@ class SingleVR extends Component {
   render() {
     return (
       <div className="stereo-link">
-        <h2 className="vr-title">{this.props.vr.title}</h2>
+        <h2 className="singleVR-title">{this.props.vr.title}</h2>
         <h3 className="vr-sub-title">{this.props.vr.subTitle}</h3>
         <a className="vr-link" target="_blank" rel="noopener noreferrer" href={this.props.vr.panoramaURL}>360</a>
         <img className="vr-img" src={this.props.vr.imagePath} alt={this.props.vr.alt} /> 
--- src/vr/VR.js
+++ src/vr/VR.js
@@ -19,22 +19,30 @@ class VR extends Component {
             <div className="textBox">
               <h2 className="emphasize">Moonshot</h2>
               <h3>Personal Design Project</h3>
-              <p>Moonshot is an unbuilt, personal interior design project for a tech start-up. Developers can have heads-down work time in the closed benching area, step out for a private call, or host meet-up events and client presentations in the residential-inspired lounge and collaboration room. Click on the image to see the space in 360 degrees and colored dots for interactivity.
+              <p>Moonshot is an unbuilt, personal interior design project for a tech start-up. Developers can have heads-down work time in the closed benching area, step out for a private call, or host meet-up events and client presentations in the residential-inspired lounge and collaboration room. Click on the VR icon on desktop to open fullscreen or on mobile with viewer for an immersive experience.
               </p>
               <p>
-              Tech: Revit | Enscape3D | Panotour
+              Tech: Revit | Enscape3D | A-Frame
               </p>
             </div>
           </div>
             <div className="container-vr">
-                <iframe title="Moonshot Virtual Reality Tour" src="http://jessegerard.com/donell-ellis/studiotk/" alt="Moonshot Virtual Reality Tour" allowFullScreen frameBorder="0"></iframe>
-                {/* http://bit.ly/donell-moonshot */}
+                <a-scene embedded>
+                  <a-assets>
+                    <img id="moonshot" crossorigin="anonymous" src="https://i.imgur.com/WI4uGQ7.jpg"></img>
+                  </a-assets>
+                  <a-sky id="background-img"  src="#moonshot"></a-sky>
+              </a-scene>
             </div>
           </div>
           <div className="vr-project-grid">
-              {list}
+              {/* {list} */}
           </div>
         </div>
+
+
+    
+
     );
   }
 }
